3172199c16b792ee30438861009bbd367922e429
Hendri Alqori <teamhendri18@gmail.com>
2025-09-16 10:25:41 +0700

    feat: remove button execute

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { Rocket, Save } from 'lucide-vue-next'
+import { Save } from 'lucide-vue-next'
 import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbSeparator } from './ui/breadcrumb'
 import { Button } from './ui/button'
 import { useWorkflowStore } from '@/stores/workflow'
@@ -8,28 +8,24 @@ const workflowStore = useWorkflowStore()
 </script>
 
 <template>
-  <header class="bg-gray-100 border-b py-3 flex items-center justify-between px-6">
-    <div>
-      <Breadcrumb>
-        <BreadcrumbList>
-          <BreadcrumbItem class="text-primary font-medium"> Workflow Definitions </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem class="font-medium text-black">
-            {{ workflowStore.workflow?.name ?? '-' }}
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
-      <h1 class="text-lg font-medium">{{ workflowStore.workflow?.name ?? '-' }}</h1>
-    </div>
-    <div class="space-x-2">
-      <Button>
-        <Rocket class="size-4" />
-        Execute
-      </Button>
-      <Button variant="outline">
-        <Save class="size-4" />
-        Save
-      </Button>
-    </div>
-  </header>
+    <header class="bg-gray-100 border-b py-3 flex items-center justify-between px-6">
+        <div>
+            <Breadcrumb>
+                <BreadcrumbList>
+                    <BreadcrumbItem class="text-primary font-medium"> Workflow Definitions </BreadcrumbItem>
+                    <BreadcrumbSeparator />
+                    <BreadcrumbItem class="font-medium text-black">
+                        {{ workflowStore.workflow?.name ?? '-' }}
+                    </BreadcrumbItem>
+                </BreadcrumbList>
+            </Breadcrumb>
+            <h1 class="text-lg font-medium">{{ workflowStore.workflow?.name ?? '-' }}</h1>
+        </div>
+        <div class="space-x-2">
+            <Button>
+                <Save class="size-4" />
+                Save
+            </Button>
+        </div>
+    </header>
 </template>
